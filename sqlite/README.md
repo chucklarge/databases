271c0490467f5b08eae1c8f9006da2ab81b467f5
Charles Clark <chucklarge@gmail.com>
2013-11-17 16:28:59 -0500

    add data

--- sqlite/README.md
+++ sqlite/README.md
@@ -1,9 +1,10 @@
 load the db
 
     sqlite users.db < users.sql
+    sqlite users.db
+    select * from users;
 
-
-commands 
+commands
 
     .databases
     .tables
--- sqlite/users.sql
+++ sqlite/users.sql
@@ -9,3 +9,9 @@ create_date INT UNSIGNED NOT NULL DEFAULT 0,
 update_date INT UNSIGNED NOT NULL DEFAULT 0,
 PRIMARY KEY (user_id)
 );
+
+insert into users values(1, 'chuck', 'close', 'cc@email.com', 1276473600, 1276473600);
+insert into users values(2, 'george', 'jones', 'gj@email.com', 1295827200, 1295827200);
+insert into users values(3, 'alex', 'smith', 'as@email.com', 1309564800, 1309564800);
+insert into users values(4, 'john', 'benotto', 'jb@email.com', 1315008000, 1315008000);
+insert into users values(5, 'peter', 'ryan', 'pr@email.com', 1323734400, 1323734400);
